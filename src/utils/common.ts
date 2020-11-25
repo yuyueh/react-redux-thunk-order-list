b8057ca0fe27d7a1d4f57bbf6166e2bec845b089
DESKTOP-DLEJLUF\yuyue <yuyueh.chang@gmail.com>
2020-11-25 10:26:55 +0800

    add debounce function

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,20 @@
+export const debounce = (fn: Function, wait: number) => {
+    let timerId: number | undefined = undefined;
+
+    function debounced() {
+        const context: any = this;
+        const args = arguments;
+
+        clearTimeout(timerId);
+
+        timerId = window.setTimeout(() => {
+            fn.apply(context, args);
+        }, wait);
+    }
+
+    return debounced;
+};
+
 export const formateDateString = (date: string) => {
     try {
         const [year, month, day] = date.split('/');
