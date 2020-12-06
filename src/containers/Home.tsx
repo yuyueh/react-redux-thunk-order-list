0f8e5e42c29cf18b899f40c5c7621b262438c224
DESKTOP-DLEJLUF\yuyue <yuyueh.chang@gmail.com>
2020-12-06 20:39:17 +0800

    remove useDebounce hooks

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import OrderListing from '../components/OrderListing';
 import { fetchOrderListing } from '../store/order/actionCreators';
 import { RootState } from '../store/types/models';
-import { useDebounce } from '../utils/hook';
+import { debounce } from '../utils/common';
 
 const Wrapper = styled.div`
     max-width: 900px;
@@ -13,16 +13,17 @@ const Wrapper = styled.div`
     margin: 0 auto;
 `;
 
+const delay = 300;
+
 const HomeComponent = () => {
     const dispatch = useDispatch();
     const orderListing = useSelector(
         (state: RootState) => state.order.orderListing
     );
     const isLoading = useSelector((state: RootState) => state.order.isLoading);
-    const debouncedFetch = useDebounce(
-        () => dispatch(fetchOrderListing()),
-        300
-    );
+    const debouncedFetch = useRef(
+        debounce(() => dispatch(fetchOrderListing()), delay)
+    ).current;
     const handleClick = () => {
         debouncedFetch();
     };
